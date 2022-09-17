cb7017c7baa6c0956b6a18814c06ad9227631e8a
hanguyen <hanguyen>
2022-09-17 08:03:58 +0700

    first day learn mui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { styled } from '@mui/system'
+import Button from '@mui/material/Button';
+const Buttonstyled = styled(Button)({
+  color: 'red'
+})
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Buttonstyled variant="outlined">Outlined</Buttonstyled>
     </div>
   );
 }
